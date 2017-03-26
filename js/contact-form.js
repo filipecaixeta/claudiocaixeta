138f8fe198901df2af0a8822e41b777122f7fca9
Filipe Caixeta <filipecaixeta@gmail.com>
2017-03-26 11:59:13 -0300

    site

---
+++ js/contact-form.js
@@ -0,0 +1,82 @@
+/* ---------------------------------------------
+ Contact form
+ --------------------------------------------- */
+$(document).ready(function(){
+    $("#submit_btn").click(function(){
+        
+        //get input field values
+        var user_name = $('input[name=name]').val();
+        var user_email = $('input[name=email]').val();
+        var user_message = $('textarea[name=message]').val();
+        
+        //simple validation at client's end
+        //we simply change border color to red if empty field using .css()
+        var proceed = true;
+        if (user_name == "") {
+            $('input[name=name]').css('border-color', '#e41919');
+            proceed = false;
+        }
+        if (user_email == "") {
+            $('input[name=email]').css('border-color', '#e41919');
+            proceed = false;
+        }
+        
+        if (user_message == "") {
+            $('textarea[name=message]').css('border-color', '#e41919');
+            proceed = false;
+        }
+        
+        //everything looks good! proceed...
+        if (proceed) {
+            //data to be sent to server
+            post_data = {
+                'userName': user_name,
+                'userEmail': user_email,
+                'userMessage': user_message
+            };
+            //Ajax post data to server
+
+            // $.ajax({
+            //     type: 'POST',
+            //     url: 'https://formspree.io/filipecaixeta@gmail.com',
+            //     crossDomain: true,
+            //     data: post_data,
+            //     dataType: 'json',
+            //     success: function(responseData, textStatus, jqXHR) {
+            //         var value = responseData.someKey;
+            //     },
+            //     error: function (responseData, textStatus, errorThrown) {
+            //         alert('POST failed.');
+            //     }
+            // });
+            email = "fiwlipewcaiwxeta@gmail.com";
+            $.post('https://formspree.io/'+.email.replace(/w/g,''), post_data, function(response){
+            
+                //load json data from server and output message     
+                if (response.type == 'error') {
+                    output = '<div class="error">' + response.text + '</div>';
+                }
+                else {
+                
+                    output = '<div class="success">' + response.text + '</div>';
+                    
+                    //reset values in all input fields
+                    $('#contact_form input').val('');
+                    $('#contact_form textarea').val('');
+                }
+                
+                $("#result").hide().html(output).slideDown();
+            }, 'json');
+            
+        }
+        
+        return false;
+    });
+    
+    //reset previously set border colors and hide all message on .keyup()
+    $("#contact_form input, #contact_form textarea").keyup(function(){
+        $("#contact_form input, #contact_form textarea").css('border-color', '');
+        $("#result").slideUp();
+    });
+    
+});
